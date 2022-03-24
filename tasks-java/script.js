9f9f43ab8ef49ca367fcf84df83d08d095a1c006
ermersonwill <ermersonpb@hotmail.com>
2022-03-23 22:40:20 -0300

    envio

---
+++ tasks-java/script.js
@@ -0,0 +1,35 @@
+// let num1 = parseInt(prompt("digite um numero"))
+// let num2 = parseInt(prompt("digite outro numero"))
+
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 / num2)
+// console.log(num1 * num2)
+
+// let nome = prompt("Escreva seu nome");
+
+// alert("Seu nome é " + nome);
+// console.log(nome);
+
+
+// let nota1 = parseInt(prompt("Digite sua primeira nota"));
+// let nota2 = parseInt(prompt("Digite sua segunda nota"));
+// console.log((nota1 + nota2)/2);
+// alert("Sua nota é " + ((nota1 + nota2)/2))
+
+// let num = parseInt(prompt("Digite um numero"))
+
+// if(num % 2 == 0){
+//     console.log("Este numero e par")
+//     alert("Esse numero e par")
+// } else {
+//     console.log("Este numero e impar")
+//     alert("Esse numero e impar")
+// }
+let num1 = parseFloat(prompt("Digite seu numero"));
+if( num1 => 0 ){
+    alert("esse numero e positivo");
+} else{
+    alert("numero invalido");
+}
+let sexo = 
